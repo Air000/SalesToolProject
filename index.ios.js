1abb0e53ea712194299bd5fcfc82a928a03bf2b3
Air <xushihan37@163.com>
2016-06-29 13:31:38 +0800

    ios version can run

--- index.ios.js
+++ index.ios.js
@@ -1,32 +1,118 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  Platform,
+  TouchableOpacity,
   StyleSheet,
+  Navigator,
+  View,
   Text,
-  View
+  Image
 } from 'react-native';
+import loginPage from './src/loginPage';
 
-class AwesomeProject extends Component {
+const defaultRoute = {
+  title: "AEMC Sales Tool - Login",
+  component: loginPage
+};
+
+class navigation extends Component {
+  _renderScene(route, navigator) {
+    let Component = route.component;
+    return (
+      <Component {...route.params} navigator={navigator} />
+    );
+  }
+  _renderNavBar() {
+    const styles = {
+      title: {
+        flex: 1, alignItems: 'center', justifyContent: 'center'
+      },
+      button: {
+        flex: 1, alignItems: 'center', justifyContent: 'center'
+      },
+      buttonText: {
+        fontSize: 17, color: '#FFFFFF', fontWeight: 'bold'
+      },
+      titleText: {
+        fontSize: 20, color: '#FFFFFF', fontWeight: 'bold'
+      },
+      icon: {
+        marginTop: 3, marginBottom: 3, width: 65, height: 43
+      }
+    }
+
+    var routeMapper = {
+      LeftButton(route, navigator, index, navState) {
+        if(index > 1) {
+          return (
+            <TouchableOpacity 
+              onPress={() => navigator.pop()}
+              style={styles.button}>
+              <Text style={styles.buttonText}>Back</Text>
+            </TouchableOpacity>
+          );
+        } else {
+          return (
+            <View style={styles.icon} >
+              <Image style={styles.icon} source={require('./imgs/logo.png')} />
+            </View>
+          );
+        }
+      },
+      RightButton(route, navigator, index, navState) {
+        if(index > 0) {
+          return (
+            <TouchableOpacity 
+              onPress={() => navigator.popToTop()}
+              style={styles.button}>
+              <Text style={styles.buttonText}>Logout</Text>
+            </TouchableOpacity>
+          );
+        } else {
+          return null
+        }
+
+      },
+      Title(route, navigator, index, navState) {
+        return (
+          <View style={styles.title}>
+            <Text style={styles.titleText}>{route.title ? route.title : 'Splash'}</Text>
+          </View>
+        );
+      }
+    };
+
+    return (
+      <Navigator.NavigationBar
+        style={{
+          alignItems: 'center',
+          backgroundColor: '#55ACEE',
+          shadowOffset:{
+              width: 1,
+              height: 0.5,
+          },
+          shadowColor: '#55ACEE',
+          shadowOpacity: 0.8,          
+          }}
+        routeMapper={routeMapper} />
+    );
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Navigator
+        initialRoute={defaultRoute}
+        configureScene={() => ({
+          ...Navigator.SceneConfigs.FloatFromBottom,
+          gestures: {}, // or null
+        })}
+        renderScene={this._renderScene}
+        sceneStyle={{paddingTop: (Platform.OS === 'android' ? 66 : 74)}}
+        navigationBar={this._renderNavBar()} />
     );
   }
 }
@@ -36,18 +122,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => navigation);
--- src/loginPage.js
+++ src/loginPage.js
@@ -29,101 +29,95 @@ class loginPage extends Component {
     })
   }  
   render() {
-    
-      var layout =
-          <View style = { styles.parent } >
-            <View style = {styles.login} >
-              <TextInput 
-                text = { this.state.username }
-                placeholder = "user name" 
-                onChangeText={(e) => {
-                  this.setState({username: e})
-                }} />
-   
-              <TextInput 
-                secureTextEntry={true}
-                text = { this.state.password } 
-                placeholder = "password"
-                onChangeText={(e) => {
-                  this.setState({password: e})
-                }} />
-              <Button
-                style={ styles.buttonStyle }
-                containerStyle={styles.buttonContainer}
-                onPress={this.handleButtonPress.bind(this)}
-              >
-                Login
-              </Button>
-            </View> 
-            
-            <View>
-              <Text>{JSON.stringify(usersDB)}</Text>
-            </View> 
-            <View style={styles.test}>
-              <TouchableOpacity style={styles.right} onPress={this._openPage.bind(this)}> 
-                <Image style={styles.poke} source={require('../imgs/poke.png')} /> 
-              </TouchableOpacity>
-            </View>
-          </View>
-      ;
-      return layout;
+    return(
+      <View style = { styles.parent } >
+        <View style = {styles.login} >
+          <TextInput 
+            style = {{height: 40}}
+            text = { this.state.username }
+            placeholder = "user name" 
+            onChangeText={(e) => {
+              this.setState({username: e})
+            }} />
+
+          <TextInput
+            style = {{height: 40}} 
+            secureTextEntry={true}
+            text = { this.state.password } 
+            placeholder = "password"
+            onChangeText={(e) => {
+              this.setState({password: e})
+            }} />
+          <Button
+            style={ styles.buttonStyle }
+            containerStyle={styles.buttonContainer}
+            onPress={this.handleButtonPress.bind(this)}
+          >
+            Login
+          </Button>
+        </View> 
+        
+        <View>
+          <Text>{JSON.stringify(usersDB)}</Text>
+        </View> 
+        <View style={styles.test}>
+          <TouchableOpacity style={styles.right} onPress={this._openPage.bind(this)}> 
+            <Image style={styles.poke} source={require('../imgs/poke.png')} /> 
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
   handleButtonPress(e) {
-      console.log('I was pressed');
-      
-      if(this.state.username in usersDB) {
-        if(usersDB[this.state.username]==this.state.password){
-          ToastAndroid.show('Login Success', ToastAndroid.SHORT)
-          this.props.navigator.push({
-            title: 'Index',
-            component: indexPage
-          })
-        }else{
-          ToastAndroid.show('Password Incorrect', ToastAndroid.SHORT)
-        }
-      } else {
-        ToastAndroid.show('User Not Found', ToastAndroid.SHORT)
+    if(this.state.username in usersDB) {
+      if(usersDB[this.state.username]==this.state.password){
+        ToastAndroid.show('Login Success', ToastAndroid.SHORT)
+        this.props.navigator.push({
+          title: 'Index',
+          component: indexPage
+        })
+      }else{
+        ToastAndroid.show('Password Incorrect', ToastAndroid.SHORT)
       }
-
-
+    } else {
+      ToastAndroid.show('User Not Found', ToastAndroid.SHORT)
+    }
   }
 };
 
 var styles = StyleSheet.create({
- 
-    // For the container View
-    parent: {
-        padding: 16,
-        flex: 1
-    },
+  parent: {
+      padding: 16,
+      flex: 1
+  },
 
-    login: {
-        flex: 5
-    },
+  login: {
+      flex: 5
+  },
 
-    test: {
-      flex: 1,
-      alignSelf: 'flex-end'
-    },
+  test: {
+    flex: 1,
+    alignSelf: 'flex-end'
+  },
 
-    buttonContainer: {
-      padding:10, 
-      height:45, 
-      overflow:'hidden', 
-      borderRadius:4, 
-      backgroundColor: 'white'
-    },
+  buttonContainer: {
+    padding:10, 
+    height:45, 
+    overflow:'hidden', 
+    borderRadius:4, 
+    backgroundColor: '#55ACEE'
+  },
 
-    buttonStyle: {
-      fontSize: 20, 
-      color: 'green'
-    },
-    poke: {
-      marginTop: 10,
-      marginBottom: 10,
-      width: 60,
-      height: 60
-    }
+  buttonStyle: {
+    fontSize: 20, 
+    color: 'white'
+  },
+  poke: {
+    marginTop: 10,
+    marginBottom: 10,
+    width: 60,
+    height: 60
+  }
 });
 
 export default loginPage;
